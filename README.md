27e485b147a6571b2904a09af2a3a59a605b78c9
garricktgh <garrick@hotmail.sg>
2019-11-23 21:05:38 +0800

    add interface and style

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# rock-paper-scissors
+# Rock! Paper! Scissors!
+
+This simple [game](https://en.wikipedia.org/wiki/Rock_paper_scissors) is done using HTML, CSS and Javascript. 
+
+# Instructions
+
+Double-click the index.html to open the game on your browser
+
--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let gameMode;
 
-let choices = {
-  rock: {name: "rock", wins: "scissors"},
-  paper: {name: "paper", wins: "rock"},
-  scissors: {name: "scissors", wins: "paper"}
+const choices = {
+  rock: { name: "Rock", wins: "Scissors" },
+  paper: { name: "Paper", wins: "Rock" },
+  scissors: { name: "Scissors", wins: "Paper" }
+}
+
+const moveOptions = ["Rock", "Paper", "Scissors"];
+
+let pVCButton = document.getElementById("pVC");
+let cVCButton = document.getElementById("cVC");
+let display = document.getElementById("display");
+let displayTwo = document.getElementById("display2");
+let playAgain = document.getElementById("playAgain")
+
+const pickMode = () => {
+  playAgain.style.display = "none";
+  pVCButton.style.display = "inline-block";
+  cVCButton.style.display = "inline-block";
+  display.innerHTML = "Pick a mode";
+  displayTwo.innerHTML = "";
 }
 
 const computerPick = () => {
-  let randomPick = Math.floor(Math.random() * 3) + 1;
+  let randomPick = Math.floor( Math.random() * 3 ) + 1;
   if (randomPick === 1) { 
     return computerChoice = "rock";
   } else if (randomPick === 2) {
@@ -18,29 +34,66 @@ const computerPick = () => {
 }
 
 const playerVsCom = () => {
-  let playerChoice = prompt("enter rock/paper/scissors");
+  gameMode = 0;
+  //hide play mode options
+  pVCButton.style.display = "none";
+  cVCButton.style.display = "none";
+  display.innerHTML = "Player VS Computer";
+  displayTwo.innerHTML = "Choose your move";
+  playerOptions();
+}
+
+//create buttons for player moves
+const playerOptions = () => {
+  let playerOptions = document.createElement('div');
+  playerOptions.setAttribute('id', 'playerOptions')
+  for ( let i = 0, len = moveOptions.length; i < len; i++ ) {
+    let playerMove = document.createElement('button');
+    playerMove.innerHTML = moveOptions[i];
+    playerMove.addEventListener('click', playerPick)
+    playerOptions.appendChild(playerMove);
+  }
+  displayTwo.appendChild(playerOptions)
+}
+
+//when player picks a move
+const playerPick = (event) => {
+  let playerChoice = event.target.innerHTML.toLowerCase();
   let computerChoice = computerPick();
-  console.log(`computer picks ${computerChoice}`);
-  console.log(`player picks ${playerChoice}`);
   checkWin(playerChoice, computerChoice);
 }
 
+//computer vs computer
 const comVsCom = () => {
+  pVCButton.style.display = "none";
+  cVCButton.style.display = "none";
+  gameMode = 1;
   let computerOne = computerPick();
-  console.log(`computer one picks ${computerOne}`);
   let computerTwo = computerPick();
-  console.log(`computer two picks ${computerTwo}`);
   checkWin(computerOne, computerTwo);
 }
 
+//check win condition
 const checkWin = (choiceOne, choiceTwo) => {
-  if(choiceOne === choiceTwo) {
-    alert("It is a tie!")
+  if (gameMode === 0 ) {
+    display.innerHTML = `Player picks ${choices[choiceOne].name}! Computer picks ${choices[choiceTwo].name}!`;
   } else {
-    if (choices[choiceOne].wins === choiceTwo) {
-      alert(`${choiceOne} wins`);
+    display.innerHTML = `Computer 1 picks ${choices[choiceOne].name}! Computer 2 picks ${choices[choiceTwo].name}!`;
+  }
+  if (choiceOne === choiceTwo) {
+      displayTwo.innerHTML = "It is a tie!";
     } else {
-      alert(`${choiceTwo} wins`);
+    if (choices[choiceOne].wins.toLowerCase() === choiceTwo.toLowerCase()) {
+      displayTwo.innerHTML = `${choices[choiceOne].name} wins!`;
+    } else {
+      displayTwo.innerHTML = `${choices[choiceTwo].name} wins!`;
     }
   }
+  playAgain.style.display = "inline-block";
 }
+
+//add event listener to buttons
+pVCButton.addEventListener("click", playerVsCom);
+cVCButton.addEventListener("click", comVsCom);
+playAgain.addEventListener("click", pickMode);
+window.addEventListener('load', pickMode);
